4be60b9c557438719df1fe906ade3ec900198dfe
Your NameGroamed <oleeof@gmail.com>
2019-01-14 14:53:01 +0300

    Correcting task

--- objectFieldSort.js
+++ objectFieldSort.js
@@ -1,4 +1,8 @@
 function objSort(arr) {
+    let newObjArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newObjArr[i] = {}
+    }
     let isNotNull = false;
     for (let key in arr[0]) {
         for (let i = 0; i < arr.length; i++) {
@@ -7,14 +11,14 @@ function objSort(arr) {
                 break
             }
         }
-        if (!isNotNull) {
-            arr.forEach(el => {
-                delete el[key]
-            })
+        if (isNotNull) {
+            for (let i = 0; i < arr.length; i++) {
+                newObjArr[i][key] = arr[i][key]
+            }
         }
         isNotNull = false
     }
-    return arr
+    return newObjArr
 }
 console.log(objSort([{ a: 1, b: 1 }, { a: 1, b: 1 }, { a: 1, b: 1 }]))
 console.log(objSort([{ a: null, b: 1 }, { a: null, b: 1 }, { a: 1, b: 1 }]))
