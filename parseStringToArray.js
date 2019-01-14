1dabb9693e0512ed32a7befdeffe58b4b5bf5c92
Your NameGroamed <oleeof@gmail.com>
2019-01-14 14:59:38 +0300

    Delete unnecessory file

--- parseStringToArray.js
+++
@@ -1,8 +0,0 @@
-function parse(str) {
-    const reg = /[а-ю]+/gi
-    const arrRes = str.match(reg)
-    return arrRes.sort(compare);
-}
-
-const compare = (a, b) => (a.toLowerCase() < b.toLowerCase()) ? -1 : (a.toLowerCase() > b.toLowerCase()) ? 1 : 0;
-console.log(parse('Привет,   как дела?'))
